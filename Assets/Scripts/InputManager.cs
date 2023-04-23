b98138571a3ca62857ac1db7e756798479b3f654
MontyCo <montycorveleyn@gmail.com>
2023-04-23 22:59:45 +0200

    Added a Camera System and the Input Manager
    Camera can be rotated using Q and E

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
@@ -14,47 +15,94 @@ public class InputManager : MonoBehaviour
 
     public bool HasPressedTab { get; private set; }
 
-    public void OnUpdate()
+    //Custom events that will be fired by the new InputSystem
+    public void OnMove(InputAction.CallbackContext context)
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        MoveInput = context.ReadValue<Vector2>();
+    }
 
-        MoveInput = new Vector2 (horizontalInput, verticalInput).normalized;
-
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            HasPressedSpace = true;
-        }
-        else
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 0)
         {
             HasPressedSpace = false;
         }
-
-        if (Input.GetKey(KeyCode.Space))
-        {
-            IsHoldingSpace = true;
-        }
         else
         {
-            IsHoldingSpace = false;
+            HasPressedSpace = true;
         }
+        HasPressedSpace = context.action.triggered;
+    }
 
-        if (Input.GetKeyDown(KeyCode.F))
-        {
-            HasPressedF = true;
-        }
-        else
+    public void OnPressF(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 0)
         {
             HasPressedF = false;
         }
-
-        if (Input.GetKeyDown(KeyCode.Tab))
-        {
-            HasPressedTab = true;
-        }
         else
+        {
+            HasPressedF = true;
+        }
+        HasPressedF = context.action.triggered;
+    }
+
+    public void OnPressTab(InputAction.CallbackContext context)
+    {
+        if (context.ReadValue<float>() == 0)
         {
             HasPressedTab = false;
         }
+        else
+        {
+            HasPressedTab = true;
+        }
+        HasPressedTab = context.action.triggered;
+    }
+
+    public void OnUpdate()
+    {
+        //Old Input
+
+        //float horizontalInput = Input.GetAxis("Horizontal");
+        //float verticalInput = Input.GetAxis("Vertical");
+
+        //MoveInput = new Vector2 (horizontalInput, verticalInput).normalized;
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    HasPressedSpace = true;
+        //}
+        //else
+        //{
+        //    HasPressedSpace = false;
+        //}
+
+        //if (Input.GetKey(KeyCode.Space))
+        //{
+        //    IsHoldingSpace = true;
+        //}
+        //else
+        //{
+        //    IsHoldingSpace = false;
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.F))
+        //{
+        //    HasPressedF = true;
+        //}
+        //else
+        //{
+        //    HasPressedF = false;
+        //}
+
+        //if (Input.GetKeyDown(KeyCode.Tab))
+        //{
+        //    HasPressedTab = true;
+        //}
+        //else
+        //{
+        //    HasPressedTab = false;
+        //}
     }
 }
