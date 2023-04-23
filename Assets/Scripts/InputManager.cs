273c3f2cf6dc50792ca1e715183fd4414a5dc68f
JoshLemonTea <jonathan.ribonnet@student.howest.be>
2023-04-23 23:15:09 +0200

    Revert "Merge branch 'main' of https://github.com/JoshLemonTea/Aztrek_Production"
    This reverts commit 12e7f17404a22c74f59fa27a612432283606178b, reversing
    changes made to 4b2f1ddb9b3f6ec25db6182490955f9d58ba1d38.

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class InputManager : MonoBehaviour
 {
@@ -15,95 +14,42 @@ public class InputManager : MonoBehaviour
 
     public bool HasPressedTab { get; private set; }
 
-    //Custom events that will be fired by the new InputSystem
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        MoveInput = context.ReadValue<Vector2>();
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if (context.ReadValue<float>() == 0)
-        {
-            HasPressedSpace = false;
-        }
-        else
-        {
-            HasPressedSpace = true;
-        }
-        HasPressedSpace = context.action.triggered;
-    }
-
-    public void OnPressF(InputAction.CallbackContext context)
-    {
-        if (context.ReadValue<float>() == 0)
-        {
-            HasPressedF = false;
-        }
-        else
-        {
-            HasPressedF = true;
-        }
-        HasPressedF = context.action.triggered;
-    }
-
-    public void OnPressTab(InputAction.CallbackContext context)
-    {
-        if (context.ReadValue<float>() == 0)
-        {
-            HasPressedTab = false;
-        }
-        else
-        {
-            HasPressedTab = true;
-        }
-        HasPressedTab = context.action.triggered;
-    }
+    public bool HasPressedTeleportKey { get; private set; } // Add teleport key property
 
     public void OnUpdate()
     {
-        //Old Input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
 
-        //float horizontalInput = Input.GetAxis("Horizontal");
-        //float verticalInput = Input.GetAxis("Vertical");
+        MoveInput = new Vector2(horizontalInput, verticalInput).normalized;
 
-        //MoveInput = new Vector2 (horizontalInput, verticalInput).normalized;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            HasPressedSpace = true;
+        }
+        else
+        {
+            HasPressedSpace = false;
+        }
 
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    HasPressedSpace = true;
-        //}
-        //else
-        //{
-        //    HasPressedSpace = false;
-        //}
+        if (Input.GetKey(KeyCode.Space))
+        {
+            IsHoldingSpace = true;
+        }
+        else
+        {
+            IsHoldingSpace = false;
+        }
 
-        //if (Input.GetKey(KeyCode.Space))
-        //{
-        //    IsHoldingSpace = true;
-        //}
-        //else
-        //{
-        //    IsHoldingSpace = false;
-        //}
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            HasPressedF = true;
+        }
+        else
+        {
+            HasPressedF = false;
+        }
 
-        //if (Input.GetKeyDown(KeyCode.F))
-        //{
-        //    HasPressedF = true;
-        //}
-        //else
-        //{
-        //    HasPressedF = false;
-        //}
-
-        //if (Input.GetKeyDown(KeyCode.Tab))
-        //{
-        //    HasPressedTab = true;
-        //}
-        //else
-        //{
-        //    HasPressedTab = false;
-        //}
         if (Input.GetKeyDown(KeyCode.Tab))
         {
             HasPressedTab = true;
