f71a1d2d9a15eff6794d8567ef63ff8838d0e5e2
MontyCo <montycorveleyn@gmail.com>
2023-04-26 20:35:14 +0200

    Changed Jumping to carry momentum and be snappier

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -24,6 +24,9 @@ public class Player : MonoBehaviour
     [SerializeField] private float _decceleration;
     public float Deceleration { get => _decceleration; set => _decceleration = value; }
 
+    [Range(0, 1)] [SerializeField] private float _airDecceleration;
+    public float AirDecceleration { get => _airDecceleration; set => _airDecceleration = value; }
+
     private Vector3 _movement;
 
     public bool IsGrounded { get { return _characterController.isGrounded; } }
@@ -33,15 +36,31 @@ public class Player : MonoBehaviour
     public void OnEnter()
     {
         _characterController = GetComponent<CharacterController>();
-        GravityValue = Physics.gravity.y;
+
+        //Increased Gravity gives snappier jumps
+        GravityValue = Physics.gravity.y * 3;
 
         transform.position = Vector3.zero;
     }
 
     public void Move(Vector3 moveInput)
     {
-        _movement.x = Mathf.MoveTowards(_movement.x, moveInput.x * MoveSpeed, Acceleration * Time.deltaTime);
-        _movement.z = Mathf.MoveTowards(_movement.z, moveInput.y * MoveSpeed, Deceleration * Time.deltaTime);
+        Vector3 relativeInput = RelativeMovementVector(moveInput);
+
+        //Apply normal movement when on ground
+        if (_characterController.isGrounded)
+        {
+            _movement.x = Mathf.MoveTowards(_movement.x, relativeInput.x * MoveSpeed, Acceleration * Time.deltaTime);
+            _movement.z = Mathf.MoveTowards(_movement.z, relativeInput.y * MoveSpeed, Deceleration * Time.deltaTime);
+        }
+
+        //Apply air movement (when input is not zero)
+        if (_characterController.isGrounded == false && moveInput.magnitude > 0)
+        {
+            _movement.x = Mathf.Lerp(_movement.x, Mathf.MoveTowards(_movement.x, relativeInput.x * MoveSpeed, Acceleration * Time.deltaTime), AirDecceleration);
+            _movement.z = Mathf.Lerp(_movement.z, Mathf.MoveTowards(_movement.z, relativeInput.y * MoveSpeed, Acceleration * Time.deltaTime), AirDecceleration);
+        }
+
         _characterController.Move(_movement * Time.deltaTime);
     }
 
@@ -79,4 +98,26 @@ public class Player : MonoBehaviour
         _movement.y = yMovement;
     }
 
+    //Function that calculates the movement input relative to the active camera
+    private Vector3 RelativeMovementVector(Vector3 moveInput)
+    {
+        float horizontalAxis = moveInput.x;
+        float verticalAxis = moveInput.y;
+
+        //camera forward and right vectors:
+        var forward = Camera.main.transform.forward;
+        var right = Camera.main.transform.right;
+
+        //project forward and right vectors on the horizontal plane (y = 0)
+        forward.y = 0f;
+        right.y = 0f;
+        forward.Normalize();
+        right.Normalize();
+
+        //this is the direction in the world space we want to move:
+        var desiredMoveDirection = forward * verticalAxis + right * horizontalAxis;
+        desiredMoveDirection.y = desiredMoveDirection.z;
+        desiredMoveDirection.z = 0;
+        return desiredMoveDirection;
+    }
 }
