747a777e19d8a777fd416a0d0677f30278535f6f
AxelAdant <114473976+AxelAdant@users.noreply.github.com>
2023-04-30 14:49:13 +0200

    Tlaloc ability implementation (1)

--- Assets/Scripts/States/TlalocState.cs
+++ Assets/Scripts/States/TlalocState.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class TlalocState : PlayerState
 {
+    private GameObject _cloudGhost;
+
+    private GameObject _cloud;
+
+    private int _abilityPressCount;
+
     public TlalocState(PlayerStateMachine playerStateMachine, InputManager inputManager, Player player) : base(playerStateMachine, inputManager, player)
     {
+        _cloudGhost = GameObject.Find("CloudGhost");
+        _cloudGhost.SetActive(false);
     }
 
     public override void OnEnter()
@@ -13,11 +21,15 @@ public class TlalocState : PlayerState
         Debug.Log("Entered Tlaloc State");
 
         base.OnEnter();
+
+        _cloud = Resources.Load<GameObject>("Cloud");
     }
 
     public override void OnUpdate()
     {
         base.OnUpdate();
+
+        TriggerAbility();
     }
 
     public override void OnExit()
@@ -25,4 +37,45 @@ public class TlalocState : PlayerState
         base.OnExit();
     }
 
+    private void TriggerAbility()
+    {
+        if (Player.IsGrounded)
+        {
+            if (InputManager.HasPressedF)
+            {
+                if (_abilityPressCount == 0)
+                {
+                    ShowCloudGhost();
+                    _abilityPressCount++;
+                }
+
+                if (_abilityPressCount > 0)
+                {
+                    HideCloudGhost();
+                    PlaceCloud();
+                    _abilityPressCount = 0;
+                }
+            }
+        }
+        else
+        {
+            HideCloudGhost();
+            _abilityPressCount = 0;
+        }
+    }
+
+    private void HideCloudGhost()
+    {
+        _cloudGhost.SetActive(false);
+    }
+
+    private void ShowCloudGhost()
+    {
+        _cloudGhost.SetActive(true);
+    }
+
+    private void PlaceCloud()
+    {
+        Object.Instantiate(_cloud, _cloudGhost.transform.position, _cloudGhost.transform.rotation);
+    }
 }
