5efea8b5d2231458faf4267b7413a0a3a3d28de4
EthanNelis <ethan.nelis@student.howest.be>
2023-05-06 13:40:48 +0200

    Added movement to tornado traps

--- Assets/Scripts/Hazards/Tornado.cs
+++ Assets/Scripts/Hazards/Tornado.cs
@@ -15,14 +15,40 @@ public class Tornado : MonoBehaviour
     [SerializeField]
     private int _damage = 1;
 
+    [SerializeField]
+    private float _moveSpeed = 2f;
+
+    [SerializeField]
+    private Transform[] _targets;
+
+    private int _currentIndex = 0;
+
     private void OnEnable()
     {
         _player = FindObjectOfType<Player>();
+
+        foreach(Transform target in _targets)
+        {
+            target.parent = null;
+        }
+    }
+
+    private void Update()
+    {
+        if (Vector3.Distance(transform.position, _targets[_currentIndex].position) < 0.1f)
+        {
+            _currentIndex++;
+            if (_currentIndex > _targets.Length - 1)
+            {
+                _currentIndex = 0;
+            }
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, _targets[_currentIndex].position, _moveSpeed * Time.deltaTime);
     }
 
     private void OnTriggerEnter(Collider other)
     {
-
         if (other.TryGetComponent(out Health health))
         {
             health.TakeDamage(_damage);
@@ -37,5 +63,15 @@ public class Tornado : MonoBehaviour
             Vector3 movement = transform.up * _windSpeed;
             _player.AddMovement(movement);
         }
+    }
+
+    private void OnBecameVisible()
+    {
+        enabled = true;
+    }
+
+    private void OnBecameInvisible()
+    {
+        enabled = false;
     }
 }
