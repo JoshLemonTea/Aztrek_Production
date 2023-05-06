7f3cb35816715f7c1df278da26554e17fa8f55e6
EthanNelis <ethan.nelis@student.howest.be>
2023-05-06 16:07:24 +0200

    Fixed camera not returning to spawnpoint on death

--- Assets/Scripts/RespawnManager.cs
+++ Assets/Scripts/RespawnManager.cs
@@ -8,13 +8,18 @@ public class RespawnManager : MonoBehaviour
 
     public Transform ActiveCheckpoint;
 
+    private Transform _cameraTarget;
+
+
     private void OnEnable()
     {
         _player = FindObjectOfType<Player>().transform;
+        _cameraTarget = GameObject.Find("CameraTarget").transform;
     }
 
     public void Respawn()
     {
         _player.position = ActiveCheckpoint.position;
+        _cameraTarget.position = ActiveCheckpoint.position;
     }
 }
