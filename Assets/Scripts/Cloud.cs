465f7a66802f1acff8caf5b6cea3894ff8a20244
yenz minnaert (Sapix) <yenzmin@gmail.com>
2023-05-15 04:02:43 +0200

    Changed Cloud model

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -4,8 +4,7 @@ using UnityEngine;
 
 public class Cloud : MonoBehaviour
 {
-    [SerializeField]
-    private float _lifeTime = 15f;
+    [SerializeField] private float _lifeTime = 15f;
 
     private void Update()
     {
