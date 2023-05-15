a016365d749cb9e1f0bda8011ec769491e7ff3c3
yenz minnaert (Sapix) <yenzmin@gmail.com>
2023-05-15 15:13:43 +0200

    Added Moving Platform

--- Assets/Scripts/RandomRotateObject.cs
+++ Assets/Scripts/RandomRotateObject.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RandomRotateObject : MonoBehaviour
+public class RandomTileRotate : MonoBehaviour
 {
     void Start()
     {
