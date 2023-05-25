78f31af05f255499a9558a9884c6d2e927853171
AxelAdant <114473976+AxelAdant@users.noreply.github.com>
2023-05-25 15:55:08 +0200

    Made a script to randomize pitch of sounds

---
+++ Assets/Scripts/SoundPitchRandomizer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public static class SoundPitchRandomizer
+{
+    private static Dictionary<AudioSource, float> audioSourceBasePitches = new Dictionary<AudioSource, float>();
+
+    public static void PlaySoundWithRandomPitch(AudioSource audioSource, AudioClip audioClip, float volumeScale, float pitchVariationRange)
+    {
+        if(!audioSourceBasePitches.TryGetValue(audioSource, out float basePitch))
+        {
+            basePitch = audioSource.pitch;
+
+            audioSourceBasePitches.Add(audioSource, basePitch);
+        }
+        
+        audioSource.pitch = basePitch + UnityEngine.Random.Range(basePitch - pitchVariationRange, basePitch + pitchVariationRange);
+
+        audioSource.PlayOneShot(audioClip, volumeScale);
+    }
+}
