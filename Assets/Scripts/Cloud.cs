9ba7b100ecab858c95658fde26ab0761fada1326
MontyCo <montycorveleyn@gmail.com>
2023-05-26 18:42:23 +0200

    Made clouds passable from the bottom

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -5,7 +5,21 @@ using UnityEngine;
 public class Cloud : MonoBehaviour
 {
     [SerializeField] private float _lifeTime = 15f;
+    private Collider collider;
 
+    private void Start()
+    {
+        Collider[] allColliders = GetComponents<Collider>();
+        for (int i = 0; i < allColliders.Length; i++)
+        {
+            if (allColliders[i].isTrigger == false)
+            {
+                collider = allColliders[i];
+            }
+        }
+
+        collider.enabled = false;
+    }
     private void Update()
     {
         CountDown();
@@ -19,5 +33,25 @@ public class Cloud : MonoBehaviour
         {
             Destroy(gameObject);
         }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            ChangeState(true);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.GetComponent<Player>() != null)
+        {
+            ChangeState(false);
+        }
+    }
+
+    private void ChangeState(bool state)
+    {
+        collider.enabled = state;
     }
 }
