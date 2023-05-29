0011813fd780888e70e2f121ddf904de3ff781a0
MontyCo <montycorveleyn@gmail.com>
2023-05-29 16:18:51 +0200

    Changed the Player Rigs

--- Assets/SetPlayerModel.cs
+++ Assets/SetPlayerModel.cs
@@ -6,7 +6,7 @@ public class SetPlayerModel : MonoBehaviour
 {
     [SerializeField] private GameObject girlModel;
     [SerializeField] private GameObject boyModel;
-    [HideInInspector] public static bool isBoy = true;
+    [HideInInspector] public static bool isBoy = false;
     // Start is called before the first frame update
     void Start()
     {
